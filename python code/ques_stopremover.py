06c238c9473167d554ce4fc0b821c16b59777907
sai <sairamprasad999@gmail.com>
2015-04-12 21:22:44 -0400

    modified ques_stopremover.py to include removing punctuation and apostrophe

--- python code/ques_stopremover.py
+++ python code/ques_stopremover.py
@@ -3,12 +3,26 @@
 import sys
 import nltk
 from nltk.corpus import stopwords
+import string
+import re
 
-#This script takes a question
+#This script takes a question removes apostophe, punctuation and stop words
 
 if len(sys.argv) != 2:
 	print "Incorrect argument format"
 else:
 	question = sys.argv[1];
+	print question;
+	#remove apostrophe
+	question = re.sub(r"'s","",question)
+	#remove punctuation
+	exclude = set(string.punctuation)
+	question = ''.join(ch for ch in question if ch not in exclude);
+	#remove stop words
 	stopword_list = stopwords.words("english");
-	print stopword_list;
+	question_words = question.split();
+	result = str();
+	for words in question_words:
+		if words not in stopword_list:
+			result += words+" "
+	print result;
