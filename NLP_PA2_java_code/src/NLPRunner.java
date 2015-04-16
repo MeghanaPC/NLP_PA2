d664f0feed0b882e8db95309097834577452cc41
trupti <entertrupti@gmail.com>
2015-04-16 17:52:43 -0400

    added code files

---
+++ NLP_PA2_java_code/src/NLPRunner.java
@@ -0,0 +1,96 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+public class NLPRunner {
+	
+	   public static void main(String[] args) {
+	    
+		  HashMap<String,String> nerMap=new HashMap<String,String>();
+	     //creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and
+	     Properties props = new Properties();
+	     boolean useRegexner = true;
+	     if (useRegexner) {
+	       props.put("annotators", "tokenize, ssplit, pos, lemma, ner, regexner");
+	      // props.put("regexner.mapping", "locations.txt");
+	     } else {
+	       props.put("annotators", "tokenize, ssplit, pos, lemma, ner");
+	     }
+	     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+	    String[] tests =
+	         {
+	             "Partial invoice (€100,000, so roughly 40%) for the consignment C27655 we shipped on 15th August to London from the Make Believe Town depot. INV2345 is for the balance.. Customer contact (Sigourney) says they will pay this on the usual credit terms (30 days)."
+	         };
+	    // String[] tests ={"then HOCKEY PLAYERS LOS ANGELES KINGS HOCKEY TEAM GRETZKY WAYNE"};
+
+	     for (String s : tests) {
+
+	       Annotation document = new Annotation(s);
+	       pipeline.annotate(document);
+
+	       List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+	       StringBuilder sb = new StringBuilder();
+	       
+	       for (CoreMap sentence : sentences) {
+	         // traversing the words in the current sentence, "O" is a sensible default to initialise
+	         // tokens to since we're not interested in unclassified / unknown things..
+	         String prevNeToken = "O";
+	         String currNeToken = "O";
+	         boolean newToken = true;
+	         for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+	           currNeToken = token.get(NamedEntityTagAnnotation.class);
+	           String word = token.get(TextAnnotation.class);
+	           // Strip out "O"s completely, makes code below easier to understand
+	           if (currNeToken.equals("O")) {
+	             // LOG.debug("Skipping '{}' classified as {}", word, currNeToken);
+	             if (!prevNeToken.equals("O") && (sb.length() > 0)) {
+	               //handleEntity(prevNeToken, sb, tokens);
+	            	 nerMap.put(sb.toString(), prevNeToken);
+	            	// System.out.println(prevNeToken+" "+sb+" ");
+	            	 sb.setLength(0);
+	               newToken = true;
+	             }
+	             continue;
+	           }
+
+	           if (newToken) {
+	             prevNeToken = currNeToken;
+	             newToken = false;
+	             sb.append(word);
+	             continue;
+	           }
+
+	           if (currNeToken.equals(prevNeToken)) {
+	             sb.append(" " + word);
+	           } else {
+	             // We're done with the current entity - print it out and reset
+	        	   nerMap.put(sb.toString(), prevNeToken);
+	        	 //  System.out.println(prevNeToken+" "+sb+" ");
+	        	   sb.setLength(0);
+	             newToken = true;
+	           }
+	           prevNeToken = currNeToken;
+	         }
+	       }
+	       
+	   
+	     }
+	     
+	     for(Entry<String, String> e:nerMap.entrySet())
+	     {
+	    	 System.out.println(e.getKey()+" = "+e.getValue());
+	     }
+	   }//main
+	 
+}
+
---
+++ NLP_PA2_java_code/src/POSTagging.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+public class POSTagging {
+	 public static void main(String[] args) throws IOException,
+     ClassNotFoundException {
+
+ // Initialize the tagger
+ MaxentTagger tagger = new MaxentTagger(
+         "tagger/english-left3words-distsim.tagger");
+
+ // The sample string
+ String sample = "Helicopters will patrol the temporary no-fly zone around New Jersey's MetLife Stadium Sunday, with F-16s based in Atlantic City ready to be scrambled if an unauthorized aircraft does enter the restricted airspace."
+ 		+ "Down below, bomb-sniffing dogs will patrol the trains and buses that are expected to take approximately 30,000 of the 80,000-plus spectators to Sunday's Super Bowl between the Denver Broncos and Seattle Seahawks."
+ 		+ "The Transportation Security Administration said it has added about two dozen dogs to monitor passengers coming in and out of the airport around the Super Bowl."
+ 		+ "On Saturday, TSA agents demonstrated how the dogs can sniff out many different types of explosives. Once they do, they're trained to sit rather than attack, so as not to raise suspicion or create a panic."
+ 		+ "TSA spokeswoman Lisa Farbstein said the dogs undergo 12 weeks of training, which costs about $200,000, factoring in food, vehicles and salaries for trainers."
+ 		+ "Dogs have been used in cargo areas for some time, but have just been introduced recently in passenger areas at Newark and JFK airports. JFK has one dog and Newark has a handful, Farbstein said.";
+
+ // The tagged string
+ /* (Adjective | Noun)* (Noun Preposition)? (Adjective | Noun)* Noun 
+ Zero or more adjectives or nouns, followed by an option group of a noun and a preposition, 
+ followed again by zero or more adjectives or nouns, followed by a single noun */ 
+ String tagged = tagger.tagString(sample);
+ List<String> phraseList=new ArrayList<String>();
+ boolean found=false;
+ String[] wordsTags=tagged.split(" ");
+ StringBuilder sb=new StringBuilder();
+ for(String wordTag:wordsTags)
+ {
+	 if(wordTag.contains("_NN"))
+	 {
+		 String[] separatedWord=wordTag.split("_");
+		 sb.append(separatedWord[0]).append(" "); 
+		 found=true;
+	 }
+	 else
+	 {
+		 if(found)
+		 {
+			 sb.setLength(sb.length()-1);
+			 phraseList.add(sb.toString()); 
+		 }
+		 found=false;
+		 sb.setLength(0);	 
+	 }
+ }
+ // Output the result
+ System.out.println(tagged);
+ for(String s:phraseList)
+ {
+	 System.out.println(s);
+ }
+}
+}
