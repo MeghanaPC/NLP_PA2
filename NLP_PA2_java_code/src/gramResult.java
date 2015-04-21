b32c178ff4fb108613d207defef3d298ccce6fb0
trupti <entertrupti@gmail.com>
2015-04-21 10:29:59 -0400

    Committing gram Result with proper noun phrase

--- NLP_PA2_java_code/src/gramResult.java
+++ NLP_PA2_java_code/src/gramResult.java
@@ -5,15 +5,21 @@ import com.google.common.collect.Multimap;
 public class gramResult {
 	  public Multimap<String, String> nerTagMap;
 	  public ArrayList<String> nounPhraseList;
-	
+	  public ArrayList<String> properNounPhraseList;
+	 // public ArrayList<String> dateList;
+	 // public ArrayList<String> numberList;
+	 
 	  gramResult()
 	  {
 		  
 	  }
-	  gramResult(Multimap<String, String> myMultimap,ArrayList<String> phrasemap)
+	  gramResult(Multimap<String, String> myMultimap,ArrayList<String> phrasemap,ArrayList<String> nounphrasemap)
 	  {
 		  nerTagMap=myMultimap;
 		  nounPhraseList=phrasemap;
+		  properNounPhraseList=nounphrasemap;
+		 // dateList=mydateList;
+		 // numberList=mynumberList;
 		  
 	  }
 	 
